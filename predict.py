8bd57aa163bf26b6f815ebc9bd1a19583ead10a7
Ben Firshman <ben@firshman.com>
2023-12-06 09:19:18 -0800

    Remove note about filter
    This is doubled up in the web interface and markdown doesn't work. I manually edited the schema in the database so we don't need to repush.

--- predict.py
+++ predict.py
@@ -117,7 +117,7 @@ class Predictor(BasePredictor):
             default=True,
         ),
         disable_safety_checker: bool = Input(
-            description="Disable safety checker for generated images. This feature is only available through the API. See [https://replicate.com/docs/how-does-replicate-work#safety](https://replicate.com/docs/how-does-replicate-work#safety)",
+            description="Disable safety checker for generated images",
             default=False,
         ),
     ) -> List[Path]:
